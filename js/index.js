8a36efb18ca4dd2c12b170fa4e09f143ab34c55d
Gianfranco Gesu Manganiello Tiso <gianfrancomanganiello@hotmail.com>
2019-08-12 03:03:43 +0200

    Version 1.0 (puro html basico)
    Signed-off-by: Gianfranco Gesu Manganiello Tiso <gianfrancomanganiello@hotmail.com>

--- js/index.js
+++ js/index.js
@@ -3,20 +3,39 @@ var reproduciendo = false
 function reproducirMusica() {
   const musica = document.getElementById('musica')
   const musica_btn = document.getElementById('musica-btn')
+  const urlBase = '../imagenes'
 
   if (!reproduciendo) {
     musica.play()
       .then(function () {
-        musica_btn.src = './imagenes/volumen-alto.png'
+        musica_btn.src = urlBase + "/volumen-alto.png"
         reproduciendo = true
       })
       .catch(function () {
-        musica_btn.src = './imagenes/mudo.png'
+        musica_btn.src = urlBase + "/mudo.png"
         reproduciendo = false
       })
   } else {
     musica.pause()
-    musica_btn.src = './imagenes/mudo.png'
+    musica_btn.src = urlBase + '/mudo.png'
     reproduciendo = false
   }
+}
+
+function goToHome() {
+  window.location.href = '../index.html'
+}
+
+function enviarCorreo(event) {
+  console.log(event)
+  event.preventDefault()
+
+  const link = `mailto:maestrosdeluniverso.blog@gmail.com` +
+    `?subject=${escape(document.getElementById('asunto').value || 'No disponible')}` +
+    `&body=${escape(`
+Nombre: ${document.getElementById('nombre').value || 'No disponible'}
+Mensaje: ${document.getElementById('mensaje').value || 'No disponible'}
+    `)}`
+
+  window.location.href = link
 }
